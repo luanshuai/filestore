00781fd984cf19c078510da030f98bf3740120d0
Christopher Hoskin <christopher.hoskin@gmail.com>
2018-06-05 16:36:32 +0100

    Experiment with firejail --force, privileged

--- install_deps.sh
+++ install_deps.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
+#echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
 
 apt-get update
 
 apt-get install vim imagemagick optipng --yes
-apt-get -t jessie-backports --no-install-recommends -y install firejail
+#apt-get -t jessie-backports --no-install-recommends -y install firejail
+
+wget -q https://sourceforge.net/projects/firejail/files/firejail/firejail_0.9.52_1_amd64.deb/download -O firejail_0.9.52_1_amd64.deb
+
+dpkg -i firejail_0.9.52_1_amd64.deb
 
 wget -q https://s3.amazonaws.com/sl-public-dev-assets/ghostscript-9.15.tar.gz -O /tmp/ghostscript-9.15.tar.gz
 cd /tmp
